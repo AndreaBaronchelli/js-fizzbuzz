38ca644135cc954456bc8f9ed6da65ad72970747
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-03-23 13:45:56 +0100

    add FizzBuzz

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/****************************************************
+ * FizzBuzz
+ ***************************************************/
+
+/* Tasks
+* 1 - ciclare i numeri da 1 a 100
+* 2 - confrontare divisibilità
+* 3 - output
+*/
+
+//1
+var numero;
+for(var i = 1; i < 100; i++) {
+    numero = i;
+    if( (numero % 3 === 0) && (numero % 5 === 0)) {
+        numero = "FizzBuzz";
+    } else if(numero % 3 === 0) {
+        numero = "Fizz";
+    } else if(numero % 5 === 0) {
+        numero = "Buzz";
+    }
+    console.log(numero);
+}
